294bb1c450725e7b2a197b84d1784e92b111cb27
CreatCodeBuild <8919794+CreatCodeBuild@users.noreply.github.com>
2021-05-26 07:00:34 +0000

    f

--- gsql-parser/src/parser.test.ts
+++ gsql-parser/src/parser.test.ts
@@ -1,14 +1,16 @@
 import { GSQLParser } from "./parser";
+import assert from 'assert/strict';
 
-(async () => {
-    const p = await GSQLParser.New();
-    
-    // Test 1
-    console.log(p.parser.parse(`SELECT t
+describe("", async () => {
+    it("", async () => {
+        const p = await GSQLParser.New();
+        // Test 1
+        console.log(p.parser.parse(`SELECT t
     FROM :s -(LIKES>:e1)- Comment:msg -(HAS_CREATOR>)- :t
     WHERE s.firstName == "Viktor" AND s.lastName == "Akhiezer"`).rootNode.lastChild?.type)
-    
-    
-    console.log(1234)
-    
-})();
+
+
+        console.log(1234)
+        assert.equal(1, 2)
+    })
+})
